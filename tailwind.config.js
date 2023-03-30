478653c6358ab20e106f667bd5efc1a6f4ce8fac
Alok <alok.dubey22@spit.ac.in>
2023-03-30 15:17:49 +0530

    install tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    // if you are using public directory then 
+    // "./app/**/*.{js,ts,jsx,tsx}",
+    // "./pages/**/*.{js,ts,jsx,tsx}",
+    // "./components/**/*.{js,ts,jsx,tsx}",
+ 
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
