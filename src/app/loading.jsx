8be74c0b1bb32a57b8497719964e54fa5df782d6
Alok <alok.dubey22@spit.ac.in>
2023-04-05 00:55:28 +0530

    add loading effect

---
+++ src/app/loading.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function loading() {
+  return (
+    <div className='flex justify-center'>
+        <img className='h-96' src="Spinner.svg" alt="loading....." />
+    </div>
+  )
+}
