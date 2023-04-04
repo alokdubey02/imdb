b254a6319fbf192219402f98f86dfe9845f6e694
Alok <alok.dubey22@spit.ac.in>
2023-04-04 15:31:18 +0530

    fetch data from tmdb api in the home page

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,26 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from './page.module.css'
+// import Image from 'next/image'
+// import { Inter } from 'next/font/google'
+// import styles from './page.module.css'
 
-const inter = Inter({ subsets: ['latin'] })
+import Results from "@/components/Results";
 
-export default function Home() {
+// const inter = Inter({ subsets: ['latin'] })
+
+const API_KEY = process.env.API_KEY
+
+export default async function Home({searchParams}) {
+  const genre = searchParams.genre || "fetchTrending";
+  const res = await fetch(`https://api.themoviedb.org/3/${genre ==="fetchTopRated" ? "movie/top_rated" : "trending/all/week"}?api_key=${API_KEY}&language=en-US&page=1`,{next:{revalidate: 1000}});
+
+  if(!res.ok){
+    throw new Error("Failed to fetch data");//this will be caught by the error page and passed to the page props
+  }
+
+  const data = await res.json();
+  const results = data.results;
+  // console.log(results);
+
   return (
-    <h1 className="text-3xl font-bold underline text-purple-300">
-      Hello world!
-    </h1>
+    <div><Results results={results}/> </div>
   )
 }
